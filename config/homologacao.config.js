893a387e7248fcdd1de8a5afe8a96e7688584d9f
Paulo Gonçalves <paulorochag@hotmail.com>
2020-10-21 18:11:05 -0300

    aula 2 - Iniciando o projeto e org. estrutura

--- config/homologacao.config.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  url: 'https://api.serverest.dev'
-}
--- test/utils/global.js
+++
@@ -1,5 +0,0 @@
-const supertest = require('supertest')
-
-const { url } = require('../../config')
-
-global.request = supertest(url)
